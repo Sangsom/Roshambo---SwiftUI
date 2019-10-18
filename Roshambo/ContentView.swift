978c2abeb30705e935c01b2e567e604d66737e03
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2019-10-18 13:36:26 +0300

    Output button hands

--- Roshambo/ContentView.swift
+++ Roshambo/ContentView.swift
@@ -9,11 +9,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    // MARK: State Properties
+    @State private var computerHand = Int.random(in: 0 ..< 3)
+    @State private var playerHand = 0
+
     // MARK: Properties
     var hands = ["Rock", "Paper", "Scissors"]
 
     var body: some View {
-        Text("Hello World")
+        VStack {
+            ForEach(hands, id: \.self) { hand in
+                Button(action: {
+                    print("Button was tapped \(hand)")
+                }) {
+                    Text(hand)
+                }
+            }
+        }
     }
 }
 
